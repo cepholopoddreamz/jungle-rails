118d923f512e2d6d3ac1ba90491f15c615d864be
cepholopoddreamz <leslie.plumb@gmail.com>
2019-02-21 01:14:59 +0000

    narrowing scope of admin categories actions

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -18,11 +18,11 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
-  def destroy
-    @category = Category.find params[:id]
-    @category.destroy
-    redirect_to [:admin, :category], notice: 'Category deleted!'
-  end
+  # def destroy
+  #   @category = Category.find params[:id]
+  #   @category.destroy
+  #   redirect_to [:admin, :category], notice: 'Category deleted!'
+  # end
 
   private
 
