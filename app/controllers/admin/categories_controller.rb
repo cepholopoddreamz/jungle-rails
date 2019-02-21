21f697dfc9863dd2ab967d47cd3a0999a51af674
cepholopoddreamz <leslie.plumb@gmail.com>
2019-02-21 01:10:22 +0000

    category routes added sucessfully and basic load of category names to html partial

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,35 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @product = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :category], notice: 'Category Created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :category], notice: 'Category deleted!'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+end
