33788d7e7aee92bbc522f11e14b75c8cf8babaa1
cepholopoddreamz <leslie.plumb@gmail.com>
2019-02-22 19:46:47 +0000

    got rid of all the bcrypt code i thought i needed to set up http authentication and found that extracting values from the env file was all that was needed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  #include BCrypt #------->>> i added this. is this right?
   def create 
     user = User.new(user_params)
+    #has_secure_password
+
     if user.save
       session[:user_id] = user.id
       redirect_to root_path
@@ -17,12 +20,6 @@ class UsersController < ApplicationController
   def logout
   end 
 
-  # def destroy
-  #   session[:user_id] = nil
-  #   redirect_to '/login'
-  # end
-
-
 private
 
 def user_params
@@ -33,7 +30,31 @@ def user_params
     :password,
     :password_confirmation
   )
-
 end
+
+# def login
+#   @user = User.find_by email(params[:email])
+#   if @user.password == params[:password]
+#     give_token
+#   else
+#     redirect_to home_url
+#   end
+# end
+
+def password
+  @password ||= Password.new(password_hash)
+end
+
+def password=(new_password)
+  @password = Password.create(new_password)
+  self.password_hash = @password
+end
+
+
+#  def destroy
+#     session[:user_id] = nil
+#     redirect_to '/login'
+#   end
+
 
 end
