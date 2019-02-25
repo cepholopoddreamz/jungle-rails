d2202e5c6b5142ae84600ad175739d0542209e9d
cepholopoddreamz <leslie.plumb@gmail.com>
2019-02-25 22:16:37 +0000

    building first tests  not just the product but the category needed to be created to then be able to pass a series of tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    before(:each) do
+      @product = Product.new()
+      @category = Category.new(name: "Anything")
+      @product.category = @category
+      @product.name = 'Chairs'
+      @product.price_cents = 234
+      @product.quantity = 20
+    end
+    it "is valid with valid attributes" do
+      expect(@product).to be_valid
+    end
+    it "is not valid without a name" do
+      @product.name = nil
+      expect(@product).to_not be_valid
+    end
+    it "is not valid without a price" do
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+    end
+    it "is not valid without a category" do
+      @product.category = nil
+      expect(@product).to_not be_valid
+    end
+    it "is not a valide quantity" do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+    end
+  end
+end
+
+#product{
+#   Product.new(name: "Anything", price: 12345,
+#   category: 'Apparel', quantity: 20 
+# }
+
+    #product {Product.new}
+
+
+#tests only the model
