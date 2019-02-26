45bcba75ffaf87dd1fe308ee9edfe2d8a05d21b6
cepholopoddreamz <leslie.plumb@gmail.com>
2019-02-26 18:30:55 +0000

    first function test figured out for onlick go to product detail page in my test the details button rather then the title link was tested

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10, #this matches the count below. if it doesn't an error is produced
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "users can navigate from the home page to the product detail pag" do
+    # ACT
+    visit root_path
+    first('.btn.btn-default').click
+
+    # # DEBUG
+    # save_screenshot
+
+    # VERIFY
+    has_content?('product-detail')
+  end
+end
